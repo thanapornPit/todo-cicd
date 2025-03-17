ccde717b70d81e4df1c797154a0d73b533431e26
Thanaporn <thanaporn8793@gmail.com GitHub>
2025-03-17 21:39:18 +0700

    Add mark as complete feature

--- src/src/App.test.js
+++ src/src/App.test.js
@@ -12,3 +12,17 @@ test('adds a todo when clicking the Add Todo button', () => {
 
   expect(screen.getByText('New todo item')).toBeInTheDocument();
 });
+
+test('marks a todo as complete when clicked', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText('Enter a todo');
+  const button = screen.getByText('Add Todo');
+
+  fireEvent.change(input, { target: { value: 'New todo item' } });
+  fireEvent.click(button);
+
+  const todoItem = screen.getByText('New todo item');
+  fireEvent.click(todoItem);
+
+  expect(todoItem).toHaveStyle('text-decoration: line-through');
+});
